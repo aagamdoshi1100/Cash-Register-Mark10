5442b661cd39f6afa03daa78dc1e1be9cd387dc9
aagamdoshi1100 <108725133+aagamdoshi1100@users.noreply.github.com>
2022-09-17 02:08:55 +0530

    fix : bug fixed

--- Cash.js
+++ Cash.js
@@ -2,7 +2,8 @@
 const billAmount = document.querySelector('#Bill-Amount');
 const cashGiven = document.querySelector('#Cash-Given');
 const checkButton = document.querySelector("#check");
-
+ // const noof = document.querySelector(".abc");
+ const cerr = document.querySelector("#mess");
 
 
 checkButton.addEventListener("click", function Clik(){
@@ -28,25 +29,126 @@ checkButton.addEventListener("click", function Clik(){
 });
 
 
-const aNotes = [2000, 500, 100, 20, 10, 5, 1];
-console.log(aNotes.length);
+// const aNotes = [2000, 500, 100, 20, 10, 5, 1];
+// console.log(aNotes.length);
 
-function FinalCashReturnToUser(finalCash){
-    const noof = document.querySelector(".abc");
-    
-    for (let i=0; i< 7; i++){
+//3450
 
-        console.log(finalCash);
-        var calci = Math.trunc(finalCash /= aNotes[i]);
-        finalCash %= aNotes[i]; 
-        console.log(calci);
-        noof[i].innerText=calci;
+function FinalCashReturnToUser(fcash){
+   
+    if (fcash > 2000){
+        Thou(fcash);
+    }else if(fcash > 500){
+        Hund(fcash);
+    }else if(fcash > 100){
+        H(fcash);
+    }else if(fcash > 20){
+        Twenty(fcash);
+    }else if(fcash > 10){
+        Tens(fcash);
+    }else if(fcash > 5){
+        Five(fcash);
+    }else if(fcash >= 1){
+        One(fcash);
     }
 
 }
 
 
 function ShowMessage(message){
- 
-    document.getElementById('mess').innerHTML = message;
+    
+    cerr.innerText = message;
+    // document.getElementById('mess').innerHTML = message;
+}
+
+function Thou(fcash){
+
+    cash = fcash /2000;
+    // console.log(cash100);
+    changes = fcash%2000; //1450
+    // console.log(changes);
+    document.getElementById('1').innerHTML = ~~cash;
+//1
+if(changes > 500){
+        Hund(changes);
+}else{
+    H(changes);
+}
+}
+
+function Hund(fcash){
+
+    cash = fcash /500;
+    // console.log(cash100);
+    changes = fcash%500;
+    // console.log(changes);
+    document.getElementById('2').innerHTML = ~~cash;
+     if(changes > 100){
+        H(changes);
+     }else{
+        Twenty(changes);
+     }
+}
+
+function H(fcash){
+
+    cash = fcash /100;
+    // console.log(cash100);
+    changes = fcash%100;
+    // console.log(changes);
+    document.getElementById('3').innerHTML = ~~cash;
+    if(changes > 20){
+        Twenty(changes);
+     }else{
+        Tens(changes);
+     }
+}
+function Twenty(fcash){
+
+    cash = fcash /20;
+    // console.log(cash100);
+    changes = fcash%20;
+    // console.log(changes);
+    document.getElementById('4').innerHTML = ~~cash;
+    if(changes > 10){
+        Tens(changes);
+     }else{
+        Five(changes);
+     }
+}
+
+function Tens(fcash){
+
+    cash = fcash /10;
+    // console.log(cash100);
+    changes = fcash%10;
+    // console.log(changes);
+    document.getElementById('5').innerHTML = ~~cash;
+    if(changes > 5){
+        Five(changes);
+     }else{
+        One(changes);
+     }
+}
+
+function Five(fcash){
+
+    cash = fcash /5;
+    // console.log(cash100);
+    changes = fcash%5;
+    console.log(changes);
+    document.getElementById('6').innerHTML = ~~cash;
+    if(changes >= 1){
+        One(changes);
+     }
+}
+
+function One(fcash){
+
+    cash = fcash /1;
+    // console.log(cash100);
+    changes = fcash%1;
+    // console.log(changes);
+    document.getElementById('7').innerHTML = ~~cash;
+
 }
